4fb5d3a5a8b9736c2f5652b98ca89705d7a6a270
tom rebibo <tom.rebibo@gmail.com>
2023-01-18 20:17:47 +0200

    adding docker agent to jenkins file

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,11 @@
-FROM ubuntu
-USER root
-WORKDIR /tmp
+FROM python:3.8
+
+WORKDIR /app
+
+COPY ./requirements.txt ./requirements.txt
+
+RUN pip install -r  requirements.txt
+
 COPY ./ ./
-RUN  apt-get update && \
-     apt-get install python3 -y && \
-     apt-get install python3-pip -y && \
-     pip3 install flask  && \
-     pip3 install gunicorn
 
-
-CMD  gunicorn -b 0.0.0.0:8000 app:app
+CMD  gunicorn -b 0.0.0.0:5000 app:app
--- app.py
+++ app.py
@@ -1,8 +1,43 @@
-from flask import Flask,redirect,url_for
-app = Flask(__name__)
-@app.route("/")
-def home():
-    return ' <h1 style="color:red;">  Mission Complete<h1>'
+import boto3, urllib
 
-if __name__ == "__main__" :
-    app.run()
+def feed(response):
+    num = len(response["Labels"])
+    counter = 0
+    for i in range(int(num)):
+        name = (response["Labels"][i]["Name"]).lower()
+        if ((name == "fish") or (name == "milk") or (name == "bread")):
+            counter += 1
+    if (counter > 0):
+        match = True
+    else:
+        match = False
+
+    return match
+def lambda_handler(event, context):
+    client = boto3.client("rekognition")
+    s3 = boto3.client("s3")
+    # get our bucket and file name
+    bucket = event['Records'][0]['s3']['bucket']['name']
+    key = urllib.parse.unquote_plus(event['Records'][0]['s3']['object']['key'], encoding='utf-8')
+
+    # get our object
+    fileObj = s3.get_object(Bucket=bucket, Key=key)
+
+    # reading file from s3 bucket and passing it as bytes
+    file_content = fileObj["Body"].read()
+
+    # passing bytes data
+    response = client.detect_labels(
+        Image={"Bytes": file_content}, MinConfidence=90
+    )
+
+    # passing s3 bucket object file reference
+    response = client.detect_labels(
+        Image={"S3Object": {"Bucket": bucket, "Name": key}},
+
+        MinConfidence=90,
+    )
+
+    # checks if the photo is milk, bread or fish and return true if it does.
+    match = feed(response)
+    return match
